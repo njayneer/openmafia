73f758dfc8e14ee67eabe75e0a5f819c56812186
njayneer <andrzejdrewnicki92@gmail.com>
2023-05-29 12:17:37 +0200

    Usprawnienie działania abortowania admina przy braku privilege

--- app/AdminPanel/decorators.py
+++ app/AdminPanel/decorators.py
@@ -1,4 +1,4 @@
-from flask import flash, redirect, url_for
+from flask import flash, redirect, url_for, abort
 from functools import wraps
 from flask_login import current_user
 
@@ -6,8 +6,7 @@ def administrator_privileges_required(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
         if current_user.name != 'admin':
-            flash('User ' + current_user.name + ' nie ma uprawnień do wykonania tej operacji!', 'alert-danger')
-            redirect(url_for('index'))
+            return abort(404)
 
         return f(*args, **kwargs)
     return decorated_function
